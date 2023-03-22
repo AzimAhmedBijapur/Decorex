f39aa4ce994fd55f80050ae19198bd2c90d5cd7d
Glitch (glitch-hello-website) <none>
2023-03-22 08:02:01 +0000

    🏥😼 Checkpoint ./.glitch-assets:50413627/374 ./index.html:50413627/416 ./chair.js:50413627/120 ./style.css:50413627/625

--- chair.js
+++ chair.js
@@ -6,7 +6,8 @@ chair.addEventListener("click",()=>{
   
   containerC.innerHTML = "";
   
-  const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574'];
+  const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574',
+                   'https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/ofiice-chair-2.glb?v=1679471498876'];
   for(let i = 0 ; i < modelUrl.length; i++){
     
   const card = document.createElement("div");
