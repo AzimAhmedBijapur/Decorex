ca3ce8292d534c6f2f25ae8055f6cc0a7abafffd
Glitch (glitch-hello-website) <none>
2023-03-22 06:39:00 +0000

    🍫🎟 Checkpoint ./script.js:50413627/2306 ./index.html:50413627/411 ./style.css:50413627/35

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ const floor = document.querySelector("#floor");
 const plant = document.querySelector("#plant");
 
 chair.addEventListener("click",()=>{
+  const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574',
+                   'https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574'];
+  for(let i = 0 ; i < modelUrl.length; i++){
+    
   const card = document.createElement("div");
   card.classList.add("card");
-  card.innerHtml="";
+  const model = document.createElement("model-viewer");
+  model.setAttribute('src',modelUrl[i]);
+  model.setAttribute('src',modelUrl[i]);
+  model.setAttribute('src',modelUrl[i]);
+  model.setAttribute('src',modelUrl[i]);
+  card.appendChild(model);
   container.appendChild(card);
+  }
 })
