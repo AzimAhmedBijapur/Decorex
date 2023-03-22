1b9ec758d7973a7386650c89e31649364a8417cb
Glitch (glitch-hello-website) <none>
2023-03-22 06:51:00 +0000

    🚠🍎 Checkpoint ./script.js:50413627/301 ./plant.js:50413627/1254 ./sofa.js:50413627/1253 ./floor.js:50413627/1252 ./table.js:50413627/202 ./index.html:50413627/1836

--- floor.js
+++ floor.js
@@ -1,12 +1,8 @@
 const container = document.querySelector(".model-container");
 
-const chair = document.querySelector("#chair");
-const sofa = document.querySelector("#sofa");
-const table = document.querySelector("#table");
 const floor = document.querySelector("#floor");
-const plant = document.querySelector("#plant");
 
-chair.addEventListener("click",()=>{
+floor.addEventListener("click",()=>{
   const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574',
                    'https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574'];
   for(let i = 0 ; i < modelUrl.length; i++){
@@ -15,9 +11,11 @@ chair.addEventListener("click",()=>{
   card.classList.add("card");
   const model = document.createElement("model-viewer");
   model.setAttribute('src',modelUrl[i]);
-  model.setAttribute('src',modelUrl[i]);
-  model.setAttribute('src',modelUrl[i]);
-  model.setAttribute('src',modelUrl[i]);
+  model.setAttribute('ar','ar');
+  model.setAttribute('camera-controls','camera-controls');
+  model.setAttribute('touch-action','pan-y');
+  model.setAttribute('ar-modes','scene-viewer');
+
   card.appendChild(model);
   container.appendChild(card);
   }
---
+++ plant.js
@@ -0,0 +1,22 @@
+const container = document.querySelector(".model-container");
+
+const plant = document.querySelector("#plant");
+
+plant.addEventListener("click",()=>{
+  const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574',
+                   'https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/office_chair.glb?v=1679465307574'];
+  for(let i = 0 ; i < modelUrl.length; i++){
+    
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const model = document.createElement("model-viewer");
+  model.setAttribute('src',modelUrl[i]);
+  model.setAttribute('ar','ar');
+  model.setAttribute('camera-controls','camera-controls');
+  model.setAttribute('touch-action','pan-y');
+  model.setAttribute('ar-modes','scene-viewer');
+
+  card.appendChild(model);
+  container.appendChild(card);
+  }
+})
