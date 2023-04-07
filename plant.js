589a878734bfdc1182e98bba109d6fcf6aab8d21
Glitch (glitch-hello-website) <none>
2023-04-07 09:38:27 +0000

    🏥🐏 Checkpoint ./.glitch-assets:50413627/1237 ./plant.js:50413627/121 ./home.html:50413627/116 ./index.html:50413627/190 ./sign-style.css:50413627/55

--- plant.js
+++ plant.js
@@ -6,7 +6,8 @@ plant.addEventListener("click",()=>{
   
   container.innerHTML = "";
   
-  const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/free_pothos_potted_plant_-_money_plant.glb?v=1679468824610'];
+  const modelUrl = ['https://cdn.glitch.global/7ede3348-1731-4eff-be67-e4d544bd7626/free_pothos_potted_plant_-_money_plant.glb?v=1679468824610',
+                   'https://cdn.glitch.me/7ede3348-1731-4eff-be67-e4d544bd7626/stylized_flower_pot.glb?v=1680859648574'];
   
   for(let i = 0 ; i < modelUrl.length; i++){
     
